eb75cfa32871807679fb52a119c92c7551b1ea40
Kenneth Davidson <xylene2016@protonmail.com>
2021-10-06 05:01:59 -0400

    Update .stop-docker.sh

--- .stop-docker.sh
+++ .stop-docker.sh
@@ -2,16 +2,22 @@
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 TZ='UTC'; export TZ
 
-/bin/systemctl daemon-reload
+/bin/systemctl daemon-reload > /dev/null 2>&1 || :
 sleep 1
-
-systemctl stop docker > /dev/null 2>&1 || :
+systemctl stop docker.socket > /dev/null 2>&1 || :
+systemctl stop docker.service > /dev/null 2>&1 || :
 systemctl stop containerd.service > /dev/null 2>&1 || :
-
-systemctl disable docker > /dev/null 2>&1 || :
+systemctl disable docker.socket > /dev/null 2>&1 || :
+systemctl disable docker.service > /dev/null 2>&1 || :
 systemctl disable containerd.service > /dev/null 2>&1 || :
+ip link set docker0 down > /dev/null 2>&1 || :
 
-ip link set docker0 down 2>/dev/null || : 
+rm -fr /run/containerd
+rm -fr /run/docker.sock
+rm -fr /run/docker
+rm -fr /var/run/containerd
+rm -fr /var/run/docker.sock
+rm -fr /var/run/docker
 
 exit
 
