885bc0cc77d429ff57958aaebc8244d000a056ff
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-25 11:12:46 -0400

    Update install-docker.sh

--- install/ubuntu/install-docker.sh
+++ install/ubuntu/install-docker.sh
@@ -15,7 +15,7 @@ set -e
 _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
 wget -q -c -t 9 -T 9 \
-'https://github.com/icebluey/docker/releases/download/v2024-10-20/containerd-1.7.23-1_amd64.tar.xz'
+'https://github.com/icebluey/docker/releases/download/v2024-10-25/containerd-1.7.23-1_amd64.tar.xz'
 wget -q -c -t 9 -T 9 \
 'https://github.com/icebluey/docker/releases/download/v2024-10-20/docker-only-27.3.1-1_amd64.tar.xz'
 rm -f /usr/bin/containerd
