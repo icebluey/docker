1d20e7acb253b6a3ddc2de4019837ab6d735cabe
Kenneth Davidson <xylene2016@protonmail.com>
2025-04-29 07:49:49 -0400

    Update dl-docker.sh

--- dl-docker.sh
+++ dl-docker.sh
@@ -41,7 +41,10 @@ _filename="$(wget -qO- 'https://download.docker.com/linux/static/stable/x86_64/'
 #_filename="$(wget -qO- 'https://download.docker.com/linux/static/stable/x86_64/' | grep 'a href="' | sed -e '/extras/d' | grep 'tgz"' | cut -d'"' -f2 | grep 'tgz$' | grep '27\.5\.' | sort -V | uniq | tail -n 1)"
 
 # 28.0.X
-_filename="$(wget -qO- 'https://download.docker.com/linux/static/stable/x86_64/' | grep 'a href="' | sed -e '/extras/d' | grep 'tgz"' | cut -d'"' -f2 | grep 'tgz$' | grep '28\.0\.' | sort -V | uniq | tail -n 1)"
+#_filename="$(wget -qO- 'https://download.docker.com/linux/static/stable/x86_64/' | grep 'a href="' | sed -e '/extras/d' | grep 'tgz"' | cut -d'"' -f2 | grep 'tgz$' | grep '28\.0\.' | sort -V | uniq | tail -n 1)"
+
+# 28.1.X
+_filename="$(wget -qO- 'https://download.docker.com/linux/static/stable/x86_64/' | grep 'a href="' | sed -e '/extras/d' | grep 'tgz"' | cut -d'"' -f2 | grep 'tgz$' | grep '28\.1\.' | sort -V | uniq | tail -n 1)"
 
 _version="$(echo "${_filename}" | sed 's/\.tgz$//g' | cut -d- -f2)"
 echo "Docker version ${_version}"
