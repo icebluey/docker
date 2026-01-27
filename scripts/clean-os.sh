e8e29c9443d63aeeefd062e9043798fe41cdb6b9
Kenneth Davidson <xylene2016@protonmail.com>
2026-01-27 08:36:46 -0500

    Update clean-os.sh

--- scripts/clean-os.sh
+++ scripts/clean-os.sh
@@ -3,98 +3,22 @@ export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 TZ='UTC'; export TZ
 apt update -y -qqq
 df -Th
-/bin/systemctl disable $(/bin/systemctl list-unit-files | grep -i -E 'docker|container|podman' | grep -iv 'container-getty' | awk '{print $1}' | sort -V | uniq | paste -sd" ")
-/bin/systemctl stop $(/bin/systemctl list-unit-files | grep -i -E 'docker|container|podman' | grep -iv 'container-getty' | awk '{print $1}' | sort -V | uniq | paste -sd" ")
 
-# delete firefox
-apt autoremove --purge -y --allow-remove-essential $(dpkg -l | grep -i -E 'firefox|firebird|google-chrome-stable' | awk '{print $2}' | sort -V | uniq | paste -sd" ")
+apt autoremove --purge -y needrestart || : 
+systemctl stop postgresql.service mysql.service mysqld.service >/dev/null 2>&1 || : 
+systemctl disable postgresql.service mysql.service mysqld.service >/dev/null 2>&1 || : 
+apt autoremove --purge -y '^postgresql.*' '^mysql.*' '^mssql.*' '^msbuild.*' '^msodbcsql.*'
+rm -fr /var/lib/postgresql /var/lib/mysql
+systemctl stop snapd.service snapd.socket >/dev/null 2>&1 || : 
+systemctl disable snapd.service snapd.socket >/dev/null 2>&1 || : 
+apt autoremove --purge -y '^php.*' '^lxd.*' '^snap.*'
+rm -fr ~/snap /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd /tmp/snap.lxd /tmp/snap-private-tmp
+systemctl stop docker.service containerd.service >/dev/null 2>&1 || : 
+systemctl disable docker.service containerd.service >/dev/null 2>&1 || : 
+apt autoremove --purge -y '^docker.*' '^container.*' '^podman.*' crun runc
+rm -fr /var/lib/docker* /var/lib/containerd /usr/libexec/docker /etc/docker /etc/containerd
+apt autoremove --purge -y --allow-remove-essential '^firefox.*' '^firebird.*' '^google.*' '^dotnet.*' '^microsoft.*' '^mono-.*' '^powershell.*' '^llvm.*'
 
-# delete microsoft
-apt autoremove --purge -y --allow-remove-essential $(dpkg -l | grep -i -E 'dotnet|microsoft|libmono|mono-|monodoc|powershell' | awk '{print $2}' | sort -V | uniq | paste -sd" ")
-
-# delete docker
-apt autoremove --purge -y --allow-remove-essential $(dpkg -l | grep -i -E 'docker|container|moby' | awk '{print $2}' | sort -V | uniq | paste -sd" ")
-apt autoremove --purge -y crun
-apt autoremove --purge -y runc
-/bin/rm -fr /etc/docker /usr/libexec/docker /etc/containerd /var/lib/containerd /var/lib/docker*
-
-# delete mysql postgresql php google-cloud
-systemctl disable postgresql.service
-systemctl disable mysql.service
-systemctl disable mysqld.service
-systemctl stop postgresql.service
-systemctl stop mysql.service
-systemctl stop mysqld.service
-apt autoremove --purge -y --allow-remove-essential $(dpkg -l | awk '$2 ~ /mysql|postgresql|google-cloud|mssql|msbuild|msodbcsql|^llvm-|^php[1-9]/ {print $2}' |  grep -iv libmysqlclient | sort -V | uniq | paste -sd" ")
-/bin/rm -fr /var/lib/postgresql /var/lib/mysql
-
-# delete snap
-snap remove --purge lxd
-snap remove --purge $(snap list | awk 'NR > 1 && $1 !~ /lxd/ && $1 !~ /snapd/ {print $1}' | sort -V | uniq | paste -sd" ")
-snap remove --purge lxd
-snap remove --purge snapd
-_services=(
-'snapd.socket'
-'snapd.service'
-'snapd.apparmor.service'
-'snapd.autoimport.service'
-'snapd.core-fixup.service'
-'snapd.failure.service'
-'snapd.recovery-chooser-trigger.service'
-'snapd.seeded.service'
-'snapd.snap-repair.service'
-'snapd.snap-repair.timer'
-'snapd.system-shutdown.service'
-)
-for _service in ${_services[@]}; do
-    systemctl stop ${_service} >/dev/null 2>&1
-done
-for _service in ${_services[@]}; do
-    systemctl disable ${_service} >/dev/null 2>&1
-done
-systemctl disable snapd.service
-systemctl disable snapd.socket
-systemctl disable snapd.seeded.service
-systemctl stop snapd.service
-systemctl stop snapd.socket
-systemctl stop snapd.seeded.service
-apt autoremove --purge -y lxd-agent-loader snapd
-/bin/rm -rf ~/snap
-/bin/rm -rf /snap
-/bin/rm -rf /var/snap
-/bin/rm -rf /var/lib/snapd
-/bin/rm -rf /var/cache/snapd
-/bin/rm -fr /tmp/snap.lxd
-/bin/rm -fr /tmp/snap-private-tmp
-/bin/rm -fr /usr/lib/snapd
-
-/bin/rm -fr /usr/share/sbt
-/bin/rm -fr /usr/share/gradle*
-/bin/rm -fr /usr/share/miniconda*
-/bin/rm -fr /usr/share/az_*
-/bin/rm -fr /usr/share/swift*
-/bin/rm -fr /usr/share/dotnet*
-/bin/rm -fr /usr/lib/snapd
-/bin/rm -fr /usr/lib/firefox
-/bin/rm -fr /usr/lib/llvm*
-/bin/rm -fr /usr/lib/mono
-/bin/rm -fr /usr/lib/jvm
-/bin/rm -fr /usr/lib/google-cloud-sdk*
-/bin/rm -fr /opt/containerd
-/bin/rm -fr /opt/mssql-tools
-/bin/rm -fr /opt/google
-/bin/rm -fr /opt/pipx
-/bin/rm -fr /opt/az
-/bin/rm -fr /opt/microsoft
-/bin/rm -fr /usr/local/sqlpackage
-/bin/rm -fr /usr/local/n
-/bin/rm -fr /usr/local/aws*
-/bin/rm -fr /usr/local/julia*
-/bin/rm -fr /usr/local/share
-/bin/rm -fr /usr/local/.ghcup
-/bin/rm -fr /opt/hostedtoolcache
-
-rm -fr /etc/apt/preferences.d/firefox*
 systemctl stop systemd-resolved.service
 systemctl stop systemd-timesyncd
 systemctl stop unattended-upgrades
@@ -103,8 +27,10 @@ systemctl disable systemd-resolved.service
 systemctl disable systemd-timesyncd
 systemctl disable unattended-upgrades
 systemctl disable udisks2.service
+
 rm -fr /etc/resolv.conf
 echo "nameserver 8.8.8.8" >/etc/resolv.conf 
+
 apt install -y chrony
 systemctl stop chrony.service
 sed -e "/^pool/d" -i /etc/chrony/chrony.conf
@@ -115,10 +41,11 @@ systemctl start chrony.service
 systemctl enable chrony.service
 sleep 10
 chronyc makestep
-apt install -y binutils coreutils util-linux findutils diffutils sed gawk grep file tar gzip bzip2 xz-utils
-apt install -y make gcc g++ m4 pkg-config perl libperl-dev groff-base dpkg-dev cmake
-apt install -y autoconf autoconf-archive autogen automake autopoint autotools-dev libtool m4 bison flex
-apt install -y libseccomp-dev libseccomp2 gperf
+
+#apt install -y binutils coreutils util-linux findutils diffutils sed gawk grep file tar gzip bzip2 xz-utils
+#apt install -y make gcc g++ m4 pkg-config perl libperl-dev groff-base dpkg-dev cmake
+#apt install -y autoconf autoconf-archive autogen automake autopoint autotools-dev libtool m4 bison flex
+#apt install -y libseccomp-dev libseccomp2 gperf
 
 df -Th
 exit
